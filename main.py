446ea32d918a9d96e17d885a3c100ec6c7bbef1d
Mathieu Ducournau <dupycournau@gmail.com>
2021-05-01 23:20:37 +0200

    mise en place parsing d'argument

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
+import sys
 import requests
+import argparse
 from bs4 import BeautifulSoup
+from search_product import search_product
 
 
 
 if __name__ == "__main__":
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument('url', help="Url of the product you search to scrap")
+    parser.add_argument(
+        "-c", "--category", help="the category you want to search", action="store_true")
+    parser.add_argument(
+        "-a", "--all", help="if you want to search all the site", action="store_true")
+    args = parser.parse_args()
+
+
+    if args.all:
+        print('Recherche sur tout le site')
+    elif args.category:
+        print("Recherche par categorie")
+    else:
+        print("Recherche par Produit")
+        product = search_product(sys.argv[1])
+        print(product['img_url'])
