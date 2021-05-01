5e625172d48f6e463661d6eb05e7865fe9a53b43
Mathieu Ducournau <dupycournau@gmail.com>
2021-05-02 00:08:56 +0200

    add functionn to write CSV from product dict

--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+import csv
+
 def format_rating(rating):
     if rating == "Five":
         return 5
@@ -9,3 +11,10 @@ def format_rating(rating):
         return 2
     else:
         return 1
+
+def dict_to_csv(book_list):
+    with open('books.csv', 'w', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=['title','desc', 'img_url', 'upc', 'price_excl', 'price_incl', 'available', 'category', 'rating', 'page_url'])
+        writer.writeheader()
+        for book in book_list:
+            writer.writerow(book)
