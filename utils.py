07978ab6f2dfb8826c2df32a2df0d9acf352aff2
Mathieu Ducournau <dupycournau@gmail.com>
2021-05-01 22:59:35 +0200

    add utils file

---
+++ utils.py
@@ -0,0 +1,11 @@
+def format_rating(rating):
+    if rating == "Five":
+        return 5
+    elif rating == "Four":
+        return 4
+    elif rating == "Three":
+        return 3
+    elif rating == "Two":
+        return 2
+    else:
+        return 1
