3d5eae0b1012b4da0d8eabf54471522b3c3fccbb
Mathieu Ducournau <dupycournau@gmail.com>
2021-05-02 19:01:34 +0200

    regex for product and tryparse for category

--- main.py
+++ main.py
@@ -1,26 +1,52 @@
-import sys
 import requests
 import argparse
+import re
 from bs4 import BeautifulSoup
-from search_product import search_product
-
+from search_product import (
+    search_product,
+    search_products_by_category,
+    search_all,
+    get_all_categories,
+)
+from utils import dict_to_csv
 
 
 if __name__ == "__main__":
+    # Parser
     parser = argparse.ArgumentParser()
-    parser.add_argument('url', help="Url of the product you search to scrap")
     parser.add_argument(
-        "-c", "--category", help="the category you want to search", action="store_true")
+        "-c", "--category", help="the category you want to search", action="store_true"
+    )
     parser.add_argument(
-        "-a", "--all", help="if you want to search all the site", action="store_true")
+        "-a", "--all", help="if you want to search all the site", action="store_true"
+    )
+    parser.add_argument("-p", "--product", help="Get all info for a product")
     args = parser.parse_args()
 
-
     if args.all:
-        print('Recherche sur tout le site')
+        book_list = search_all()
+        dict_to_csv(book_list, "all_books")
     elif args.category:
-        print("Recherche par categorie")
-    else:
-        print("Recherche par Produit")
-        product = search_product(sys.argv[1])
-        print(product['img_url'])
+        list_categories = get_all_categories()
+        for index, category in enumerate(list_categories):
+            print(f"[{index}] {category['label']}")
+        choice = input(
+            "Choississez le numero de la categorie que vous voulez chercher : "
+        )
+        try:
+            choice = int(choice)
+            result = search_products_by_category(list_categories[choice]["url"])
+            dict_to_csv(result, f"{list_categories[choice]['label']}_books")
+        except IndexError:
+            print("Le chiffre entrée n'existe pas")
+        except ValueError as err:
+            print("Vous n'avez pas entré un chiffre")
+    elif args.product:
+        if args.product != "":
+            if re.search(r"^(https:\/\/books.toscrape.com\/catalogue\/)[\w\W]*(index.html)$", args.product):
+                product = [search_product(args.product)]
+                dict_to_csv(product, product[0]['title'][:10] + '.csv')
+            else:
+                print("Vous n'avez pas envoyé un lien de book.toscrape")
+        else:
+            print("Vous n'avez pas entré d'url")
