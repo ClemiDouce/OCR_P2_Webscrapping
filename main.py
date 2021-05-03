199c976e715b6d41b63f4bc9065706e2b0aa05e4
Mathieu Ducournau <dupycournau@gmail.com>
2021-05-03 12:02:47 +0200

    add zip argument and call to zip function

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from search_product import (
     search_all,
     get_all_categories,
 )
-from utils import dict_to_csv
+from utils import dict_to_csv, zip_files
 import settings
 
 
@@ -21,14 +21,14 @@ if __name__ == "__main__":
     parser.add_argument(
         "-a", "--all", help="if you want to search all the site", action="store_true"
     )
+    parser.add_argument("-p", "--product", help="Get all info for a product")
     parser.add_argument(
-        "-p", "--product", help="Get all info for a product"
+        "-i", "--image", help="Download image of each product", action="store_true"
     )
-    parser.add_argument(
-        "-d", "--download", help="Download image of each product", action="store_true"
-    )
+    parser.add_argument("-z", "--zip", help="Zip img and csv", action="store_true")
     args = parser.parse_args()
-    settings.download_image = args.download
+    settings.download_image_option = args.download
+    settings.zip_option = args.zip
 
     if args.all:
         book_list = search_all()
@@ -42,7 +42,10 @@ if __name__ == "__main__":
         try:
             choice = int(choice)
             result = search_products_by_category(list_categories[choice]["url"])
-            dict_to_csv(result, f"{list_categories[choice]['label']}_books")
+            categorie_choice = list_categories[choice]["label"]
+            dict_to_csv(result, f"{categorie_choice}_books")
+            if settings.zip_option == True:
+                zip_files(f"{categorie_choice}_results")
         except IndexError:
             print("Le chiffre entrée n'existe pas")
         except ValueError as err:
@@ -54,10 +57,13 @@ if __name__ == "__main__":
                 args.product,
             ):
                 product = [search_product(args.product)]
+                product_title = product[0]["title"][:10]
                 dict_to_csv(product, product[0]["title"][:10] + ".csv")
+                if settings.zip_option == True:
+                    zip_files(product_title)
             else:
                 print("Vous n'avez pas envoyé un lien de book.toscrape")
         else:
             print("Vous n'avez pas entré d'url")
     else:
-        print("Vous n'avez entré aucun argument")
+        print("Vous avez entré aucun argument")
